680b0329c166d572039fd516a70f4944c939d93d
Aadi Kulshrestha <aadi.kulsh@gmail.com>
2024-11-23 11:04:11 -0500

    input form

--- src/App.tsx
+++ src/App.tsx
@@ -4,31 +4,43 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [inputs, setInputs] = useState({prescription: "", quantity: 0})
+  
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    alert(inputs);
+  }
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <form onSubmit={handleSubmit}>
+      <label>Enter Prescription:
+      <input 
+        type="text" 
+        name="prescription" 
+        value={inputs.prescription}
+        onChange={handleChange}
+      />
+      </label>
+      <label>Enter Quantity:
+      <input 
+        type="number" 
+        name="quantity" 
+        min="0"
+        step="1"
+        value={inputs.quantity}
+        onChange={handleChange}
+      />
+      </label>
+      <input type="submit" />
+    </form>
   )
 }
 
