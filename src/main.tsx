fcc0284433505c583879df96468a7122ce01d8d8
Aadi Kulshrestha <aadi.kulsh@gmail.com>
2024-11-23 13:56:00 -0500

    Page selection

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
-import App from "./App";
+import Table from "./pages/Table.tsx";
+import Layout from "./pages/Layout.tsx"
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Table />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
---
+++ src/pages/Table.tsx
@@ -0,0 +1,14 @@
+import OrderForm from "../OrderForm";
+import InventoryTable from "../InventoryTable";
+
+import "./Table.css";
+
+function Table() {
+  return (
+    <div>
+      <InventoryTable></InventoryTable>
+    </div>
+  );
+}
+
+export default Table;
